7f99ab087ee96c7888021cef09e146abe9de6953
Kevin Pearson Lebreux <6153760@cegepmontpetit.ca>
2022-06-10 10:52:05 -0400

    INIT Création projet Départ
    Initialisation de du projet.

---
+++ S08_DependencyInjection/S08_DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using S08_DependencyInjection_Models;
+using S08_DependencyInjection_Models.ViewModels;
+using S08_DependencyInjection_Services.Interfaces;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace S08_DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IServiceAbc _svcAbc;
+        private readonly IServiceXyz _svcXyz;
+
+
+        public HomeController(ILogger<HomeController> logger, IServiceAbc svcAbc, IServiceXyz svcXyz)
+        {
+            _logger = logger;
+            _svcAbc = svcAbc;
+            _svcXyz = svcXyz;
+        }
+
+        public IActionResult Index()
+        {
+            _svcAbc.DoSomething();
+            _svcXyz.DoSomething();
+
+            return View(new HomeIndexVM(new List<MonitoringServiceInfosVM> { _svcAbc.GetInfos, _svcXyz.GetInfos }));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ S08_DependencyInjection/S08_DependencyInjection/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using S08_DependencyInjection_Services;
+using S08_DependencyInjection_Services.Interfaces;
+
+namespace S08_DependencyInjection
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ISingletonService>(new SingletonService());
+            services.AddScoped<IScopedService, ScopedService>();
+            services.AddTransient<ITransientService, TransientService>();
+
+            services.AddTransient<IServiceAbc, ServiceAbc>();
+            services.AddTransient<IServiceXyz, ServiceXyz>();
+
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
